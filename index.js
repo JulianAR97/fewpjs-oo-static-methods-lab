fd036367a65f3297e30bd0909269824b3597c03b
Julian <julianrosenthal@yahoo.com>
2021-03-03 19:20:14 -0700

    commit tree fdec2c4d7c05fd8b5c0e605cb16fe0489129a89c

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ''']+/g, '')
+  }
+
+  static titleize(string) {
+    let words = string.split(' ')
+    let newWords = [];
+    let firstWord = true
+    const noCapWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let word of words) {
+      if (noCapWords.indexOf(word) === -1 || firstWord) {
+        firstWord = false
+        newWords.push(this.capitalize(word))
+      } else {
+        newWords.push(word)
+      }
+    }
+    return newWords.join(' ')
+  }
 }
